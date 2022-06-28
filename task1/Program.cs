f4778b398ad0b95538057792a247df1b315b0373
Andrey Vedischev <vedischev76@mail.ru>
2022-06-28 20:12:42 +0500

    add task 1 get random array

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,3 +1,22 @@
 ﻿using System;
 using static System.Console;
 Clear();
+
+Write("Введите размер матрицы через пробел: ");
+string[] sizeS = ReadLine().Split(' ',StringSplitOptions.RemoveEmptyEntries);
+int rows = int.Parse(sizeS[0]);
+int columns = int.Parse(sizeS[1]);
+
+
+int[,] GetRandomArray(int row, int column)
+{
+    int[,] result = new int[row,column];
+    for(int i = 0; i < result.GetLength(0); i++)
+    {
+        for(int j = 0; j < result.GetLength(1); j++)
+        {
+            result[i, j]= new Random().Next(-10,10);
+        }
+    }
+    return result;
+}
