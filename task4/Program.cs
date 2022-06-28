237fff1d4d32a14075024df9f26258ac9bffcc26
Andrey Vedischev <vedischev76@mail.ru>
2022-06-28 23:28:39 +0500

    add task 4 foreach

---
+++ task4/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using static System.Console;
+Clear();
+
+int[] array = { 1, 2, 3, 4, 5 };
+int max = array[0];
+int min = array[0];
+
+foreach (int r in array)
+{
+    max = max < r ? r : max;
+    min = min > r ? r : min;
+}
+
+WriteLine($"{min} {max}");
+
+
