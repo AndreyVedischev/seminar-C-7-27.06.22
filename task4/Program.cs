82ba751e73af1e736381a72820be3c9910e1b1a5
Andrey Vedischev <vedischev76@mail.ru>
2022-06-28 23:58:56 +0500

    add task 4 foreach array

--- task4/Program.cs
+++ task4/Program.cs
@@ -5,13 +5,29 @@ Clear();
 int[] array = { 1, 2, 3, 4, 5 };
 int max = array[0];
 int min = array[0];
+int sum = 0;
+int count = 0;
 
-foreach (int r in array)
+foreach (var r in array)
 {
     max = max < r ? r : max;
     min = min > r ? r : min;
+    sum += r;
+    count += r % 2 == 0 ? 1 : 0;
 }
 
-WriteLine($"{min} {max}");
+WriteLine($"{min} {max} {sum} {count}");
+WriteLine(String.Join(",",GetArrayFrom("1 2 3")));
 
-
+int[] GetArrayFrom(string strAr)
+{
+    string[] arS = strAr.Split(' ',StringSplitOptions.RemoveEmptyEntries);
+    int[] result = new int[arS.Length];
+    int i = 0;
+    foreach (var item in arS)
+    {
+        result[i] = int.Parse(item);
+        i++;
+    }
+    return result;
+}
