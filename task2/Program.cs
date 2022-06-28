30d9989178a4691275a3019240ffb2fb0a02d4af
Andrey Vedischev <vedischev76@mail.ru>
2022-06-28 21:00:42 +0500

    add task 2 tabie m + n

---
+++ task2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using static System.Console;
+Clear();
+
+Write("Введите число строк массива: ");
+int m = int.Parse(ReadLine());
+Write("Введите число столбцов массива: ");
+int n = int.Parse(ReadLine());
+int[,] array = FillArrayMPlusN(m, n);
+PrintArray(array);
+
+
+int[,] FillArrayMPlusN(int rows, int len)
+{
+    int[,] array1 = new int[rows, len];
+    for(int i = 0; i < array1.GetLength(0); i++)
+    {
+        for(int j = 0; j < array1.GetLength(1); j++)
+        {
+            array1[i, j]= i + j;
+        }
+    }
+    return array1;
+}
+
+void PrintArray(int[,] array1)
+{
+    for(int i = 0; i < array1.GetLength(0); i++)
+    {
+        for(int j = 0; j < array1.GetLength(1); j++)
+        {
+            Write($"{array1[i,j]} ");            
+        }
+        WriteLine();
+    }    
+}
