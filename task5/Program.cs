e232a125b4679f0d4de7e585a73f5a58a6b11584
Andrey Vedischev <vedischev76@mail.ru>
2022-06-30 21:52:21 +0500

    add task sum diagonal array

---
+++ task5/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using static System.Console;
+Clear();
+
+Write("Введите число строк массива: ");
+int m = int.Parse(ReadLine());
+Write("Введите число столбцов массива: ");
+int n = int.Parse(ReadLine());
+int[,] array = GetRandomArray(m, n);
+PrintArray(array);
+WriteLine($"{SumDigArray(array)}");
+
+
+int[,] GetRandomArray(int row, int column)
+{
+    int[,] result = new int[row,column];
+    for(int i = 0; i < result.GetLength(0); i++)
+    {
+        for(int j = 0; j < result.GetLength(1); j++)
+        {
+            result[i, j]= new Random().Next(0,10);
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] array1)
+{
+    for(int i = 0; i < array1.GetLength(0); i++)
+    {
+        for(int j = 0; j < array1.GetLength(1); j++)
+        {
+            Write($"{array1[i,j]} ");            
+        }
+        WriteLine();
+    }    
+}
+
+int SumDigArray(int[,] arr)
+{
+    int sum = 0;
+    for(int i = 0; i < arr.GetLength(0);i++)
+    {
+        sum += arr[i,i];        
+    }
+    return sum;
+}
